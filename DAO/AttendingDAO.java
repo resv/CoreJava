07e1c856b0d52b4656fab962e15b610526035088
Adam Kim <akim456@gmail.com>
2018-07-29 04:04:47 -0400

    been up 24 hours, ok my brain stopped working,  I should sleep my last 2 remaining hours for work.

--- DAO/AttendingDAO.java
+++ DAO/AttendingDAO.java
@@ -3,20 +3,55 @@ package CoreJava.DAO;
 import CoreJava.Models.Attending;
 import CoreJava.Models.Course;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class AttendingDAO {
 
-    public List<Attending> getAttending(){
-
+	
+	/*INITIALIZING FILE*/
+	String directory = ("C:\\Users\\akim4\\Google Drive\\CODE\\JAVAPROGRAMS\\PERSCHOLAS PLATFORM JD WORKSPACE\\src\\CoreJava\\Models");
+	String specific = ("\\FILE_BANK\\attending.csv");
+	File aFile = new File(directory + specific);
+	
+    public List<Attending> getAttending() throws FileNotFoundException{    	
+    	Scanner aReader = new Scanner(aFile);
+    	ArrayList<Attending> alist = new ArrayList<Attending>(); /*CREATES ARRAYLIST TO STORE INFORMATION FROM ATTENDING CSV (aFile)*/
+    	
+    	while (aReader.hasNextLine()) {
+    		String data[] = aReader.nextLine().split(",");
+    		for (int i = 0; i < data.length; i++) {
+    			alist.add(new Attending(Integer.parseInt(data[i]),data[i+1]));
+    		}
+    	}
+    	aReader.close();
+		return alist;
     }
-
+    
     public void registerStudentToCourse(List<Attending> attending, String studentEmail, int courseID){
-
+//    	Boolean counter = false;
+    	for (Attending alist : attending) {
+    		if(studentEmail == alist.getStudentEmail() && courseID == alist.getCourseID()) {
+//    			counter = true;
+    		} else {
+//    		if(counter == false) {
+    			attending.add(new Attending(courseID,studentEmail));
+    			saveAttending(attending);
+//    		}
+    		}
+    	}
     }
 
     public List<Course> getStudentCourses(List<Course> courseList, List<Attending> attending, String studentEmail){
-
+    	ArrayList<Course> courseList = new Array<Course>();
+    	for(Attending alist : attending) {
+    		if (studentEmail == attending.get())
+    	}
+    	
+    	
     }
 
     public void saveAttending(List<Attending> attending){
--- Models/Attending.java
+++ Models/Attending.java
@@ -2,19 +2,35 @@ package CoreJava.Models;
 
 public class Attending {
 
-    public void setCourseID(int courseID){
+	
+	private int courseID;
+	private String studentEmail;
+	
+	
+    public Attending() {
+    	
+	}
 
-    }
+	public Attending(int courseID, String studentEmail) {
+		this.courseID = courseID;
+		this.studentEmail = studentEmail;
+	}
 
-    public int getCourseID(){
+	public int getCourseID() {
+		return courseID;
+	}
 
-    }
+	public void setCourseID(int courseID) {
+		this.courseID = courseID;
+	}
 
-    public void setStudentEmail(String studentEmail){
+	public String getStudentEmail() {
+		return studentEmail;
+	}
 
-    }
+	public void setStudentEmail(String studentEmail) {
+		this.studentEmail = studentEmail;
+	}
 
-    public String getStudentEmail(){
-
-    }
+	
 }
