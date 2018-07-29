f7a516da3cf64ca05b8f9511d1bdbeccc0bf006a
Adam Kim <akim456@gmail.com>
2018-07-28 23:39:45 -0400

    cleaned up comments!

--- DAO/StudentDAO.java
+++ DAO/StudentDAO.java
@@ -3,32 +3,37 @@ package CoreJava.DAO;
 import CoreJava.Models.Student;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class StudentDAO {
 	
-    public List<Student>  getStudents(){
-    	/*INITIALIZING FILE*/
-    		String directory = ("C:\\Users\\akim4\\Google Drive\\CODE\\JAVAPROGRAMS\\PERSCHOLAS PLATFORM JD WORKSPACE\\src\\CoreJava\\Models");
-    		String specific = ("\\FILE_BANK\\students.csv");
-    		File sFile = new File(directory + specific);
+	public List<Student>  getStudents() throws FileNotFoundException{
+		/*INITIALIZING FILE*/
+    	String directory = ("C:\\Users\\akim4\\Google Drive\\CODE\\JAVAPROGRAMS\\PERSCHOLAS PLATFORM JD WORKSPACE\\src\\CoreJava\\Models");
+    	String specific = ("\\FILE_BANK\\students.csv");
+    	File sFile = new File(directory + specific);
     		
     	/*INITIALIZING SCANNER*/	
-    		Scanner sReader = new Scanner(sFile);
-    		ArrayList<Student> slist = new ArrayList<Student>(); /*CREATES ARRAYLIST TO STORE INFORMATION FROM STUDENTS CSV (sFile)*/
-    	/*WHILE LOOP TO READ FILE*/
-    		while (sReader.hasNextLine()) {
-    			String data[] = sReader.nextLine().split(","); /* PUTS THE INFORMATION INTO A VARIABLE*/
-    			slist.add(new Student(data[0], data[1], data[2]));    /*VARIABLE IS SPLIT BY ",", ADDED INTO ARRAY LIST*/
-    			}
-    		return slist;
-    		sReader.close();
-    		}
+    	Scanner sReader = new Scanner(sFile);
+    	ArrayList<Student> slist = new ArrayList<Student>(); /*CREATES ARRAYLIST TO STORE INFORMATION FROM STUDENTS CSV (sFile)*/
     		
-    	/*CLOSES READER*/
-
+    	/*WHILE LOOP TO READ FILE*/
+    	while (sReader.hasNextLine()) {
+    		String data[] = sReader.nextLine().split(","); /* PUTS THE INFORMATION INTO A VARIABLE SPLIT BY ","*/
+    		for (int i = 0; i < data.length; i++) {
+    			slist.add(new Student(data[i], data[i+1], data[i+2]));    /*CREATES STUDENT OBJECT WITH DATA FROM CSV*/
+    		}
+    	}
+    	sReader.close(); /*CLOSES READER*/
+    	
+    	/*RETURNS OBJECTS FROM LISTARRAY*/
+    	return slist;
+    }
+    		
+    	
     public Student getStudentByEmail(List<Student> studentList, String studentEmail){
 
     	
