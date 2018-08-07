58e50d6b9209e264afe0f1d652462895a79569bb
Adam Kim <akim456@gmail.com>
2018-08-07 18:04:47 -0400

    Update TestRunner.java

--- MainEntryPoint/TestRunner.java
+++ MainEntryPoint/TestRunner.java
@@ -4,8 +4,8 @@ public class TestRunner {
     public static void main(String[] args) {
 //        Many things to consider working on this in the future and things I've learned.
 //        You can create an array string on the fly
-//        equality comparison with emails or another string should use .equals due to "@"char
-//        concurrent exception errors are and should be used with an irirator or boolean out 
+//        equality comparison with emails or another string should use .equals due to "==" is referencing and not checking for value equality
+//        concurrent exception errors are and should be used with an iterator or boolean out 
 //        
     }
 }
